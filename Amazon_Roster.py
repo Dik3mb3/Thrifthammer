086ed22672777eb7e6c423fe42de7e842bae8744
Alexander Alonso <alexanderalonso1996@gmail.com>
2025-12-28 12:42:32 -0500

    Added Amazons

---
+++ Amazon_Roster.py
@@ -0,0 +1,16 @@
+#Local 2025 Roster Rules 
+Name = {'Lineman': 'Eagle Warrior', 'Blitzer': 'Piranha Warrior', 'Thrower': 'Python Warrior', 'Blocker': 'Jaguar Warrior'}
+Cost = {'Lineman': 50000, 'Blitzer': 90000, 'Thrower': 80000, 'Blocker': 110000}
+Stats = {'Lineman': {'MA': 6, 'ST': 3, 'AG': 3, 'PA': 4, 'AV': 8},
+         'Thrower': {'MA': 6, 'ST': 3, 'AG': 3, 'PA':3, 'AV': 8},
+         'Blitzer': {'MA': 7, 'ST': 3, 'AG': 3, 'PA': 4, 'AV': 8},
+         'Blocker': {'MA':4, 'ST': 5, 'AG': 5, 'PA': 6, 'AV': 9}
+         }
+Skills = {'Lineman': ['Dodge'],
+          'Thrower': ['Dodge', 'On The Ball', 'Pass', 'Safe Pass'],
+          'Blitzer': ['Dodge', 'Hit and Run', 'Jump Up'],
+          'Blocker': ['Defensive','Dodge']
+}
+
+P_Access = {'Lineman': 'G', 'Thrower': 'GP', 'Blitzer': 'AG', 'Blocker': 'GS'}
+S_Access = {'Lineman': 'AS', 'Thrower': 'AS', 'Blitzer': 'S', 'Blocker': 'A'}
