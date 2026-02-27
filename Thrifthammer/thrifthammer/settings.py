d1476bfb636939128a84c88d5d832d719329b572
Alexander Alonso <alexanderalonso1996@gmail.com>
2026-02-26 20:53:54 -0500

    Make site live try 1

--- Thrifthammer/thrifthammer/settings.py
+++ Thrifthammer/thrifthammer/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get(
     'django-insecure-change-me-in-production'
 )
 
-DEBUG = os.environ.get('DJANGO_DEBUG', 'True').lower() in ('true', '1', 'yes')
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False').lower() in ('true', '1', 'yes')
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
